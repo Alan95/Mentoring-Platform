3d4a03a3c7a98f8f4d16b0f93ca7a66eb53e46c7
Alan Saber <Alan Saber>
2017-11-02 17:21:15 +0100

    Intial State

---
+++ resources/assets/js/components/Home.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="container">
+        <div class="jumbotron">
+            <h1 class="display-3">Mentoring Platform</h1>
+            <p class="lead">The best platform.</p>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
